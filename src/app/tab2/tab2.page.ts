b7b0c81930fa4e36cfe41a82655bed1320dcde14
Lukas Pol <lukaspaespol@gmail.com>
2021-03-11 22:38:11 -0300

    create page for cart

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { BookService } from './../services/book.service';
+import { Component, OnInit } from '@angular/core';
+import { Book } from '../models/book';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page  implements OnInit{
 
-  constructor() {}
+  private books: Book[] = [];
 
+  constructor(private bookService: BookService) {}
+
+  ngOnInit(): void {
+    this.bookService.books.subscribe(books => {
+      this.books = books;
+    });
+  }
+
+  ionViewDidEnter() {
+    this.getBooksCart();
+    console.log(this.books);
+  }
+
+  getBooksCart() {
+    this.books = this.bookService.getBooksFromStorage();
+    console.log(this.books);
+  }
+
+  addCountBook(book: Book) {
+    this.bookService.addToCart(book);
+    this.bookService.changeBook();
+
+  }
+
+  removeBook(book: Book){
+    this.bookService.removeToCart(book);
+    this.bookService.changeBook();
+  }
 }
