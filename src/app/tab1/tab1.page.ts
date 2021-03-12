21b5e39fa0c32738121ba6240466a2ca3efdc41c
Lukas Pol <lukaspaespol@gmail.com>
2021-03-11 22:37:37 -0300

    Create page home

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Book } from './../models/book';
+import { BookService } from '../services/book.service';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
 
-  constructor() {}
+  public books: Book[] = [];
 
+  constructor(private bookService: BookService) {}
+
+  ngOnInit(): void {
+    this.getBooks();
+    console.log(this.books);
+  }
+
+  getBooks() {
+    this.books = this.bookService.getBooks();
+  }
+
+  addToCart(book: Book) {
+    this.bookService.addToCart(book);
+  }
 }
